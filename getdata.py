cc3b14d12c1e6f382922f33db66befe8fa1ae2d2
Suyeon Son <suyeon.son@icloud.com>
2014-10-20 15:48:44 -0500

    produce json file of all courses for a specified term

--- getdata.py
+++ getdata.py
@@ -15,16 +15,29 @@ API_KEY = "1kekecMN31fxge0j"
 BASE_URL = "http://api.asg.northwestern.edu/"
 
 def api_call(url_path, **params): # function to get api response with specified parameters
-
 	params["key"] = API_KEY
 	response = requests.get(urljoin(BASE_URL,url_path), params = params)
 	return response.json()
-
-def terms():
+def terms(): 
 	return api_call("terms")
-
 def subjects(term_id):
-	return api_call("subjects",term = term_id)
+	return api_call("subjects", term = term_id)
+def courses(term_id, subject_code):
+	return api_call("courses", subject = subject_code, term = term_id)	
+
+all_terms = list(reversed((sorted(terms(), key = lambda x: x["start_date"])))) # sort the json of all terms by their start date, reverse the order so most recent appears earliest, turn into list
+
+latest_term_id = all_terms[0]["id"] # get unique id of the latest term
+
+all_subjects = subjects(latest_term_id) # get json of all subjects offered for the latest term
+
+subject_codes = [] # create a list of all the subject codes
+for subject in all_subjects:
+	subject_codes.append(subject["symbol"])
+
+for subject_code in subject_codes: # produce json of all courses offered in the latest term
+	f = open('courses.json', 'a')
+	print >> f, courses(latest_term_id, subject_code)
 
 
 
